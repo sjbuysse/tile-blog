c99e5ea12f5939b89698434d9c046c2167f491c3
Simon Buysse <buysse.simon@gmail.com>
2015-08-10 12:21:22 +0200

    adjusted scroll.js so it works with #top, added article styling, added icons

--- scroll.js
+++ scroll.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
     var navbarHeight = $(".navbar").css("height");
     navbarHeight = navbarHeight.replace("px","");
-    $('a[href^="#"]').on('click', function(event) {
-        var target = $( $(this).attr('href')  );
-        if( target.length ) {
+    $('a[href*="#"]').on('click', function(event) {
+        var target = $(this).attr('href').replace("index.html", "");
+        var $target = $(target);
+        console.log($target.length)
+        if( $target.length ) {
             event.preventDefault();
-            var difference = (target.offset().top  - navbarHeight);
+            var difference = ($target.offset().top  - navbarHeight);
+            if (target == "#top") {
+                difference = 0;
+            }
+            console.log(difference);
             $('html, body').animate({
                 //offset().top returns the offset of the element that calls it relative to the top.
                 //we call the scrollTop on the html and body element (use both elements for browser support)
